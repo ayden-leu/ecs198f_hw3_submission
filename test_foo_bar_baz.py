dd765e61687549248182c6736638c26d4d0fc0be
ayden-leu <ajleu@ucdavis.edu>
2026-02-28 01:45:23 -0800

    ok now im just confused

--- test_foo_bar_baz.py
+++ test_foo_bar_baz.py
@@ -4,14 +4,20 @@ from foo_bar_baz import foo_bar_baz
 
 def test_zero():
 	result:str = foo_bar_baz(0)
-	assert(result != ""), \
-		"foo_bar_baz(0) should have handled the zero or raised an error."
+	assert(type(result) is str), \
+		"foo_bar_baz(" + str(i) + ") should return a string.  Got: " + str(type(result))
+
+	assert(result == ""), \
+		"Expected: ||\n     Got: |" + result + "|"
 
 def test_negatives():
 	for i in range(-100, 0):
 		result:str = foo_bar_baz(i)
-		assert(result != ""), \
-			"foo_bar_baz(" + str(i) + ") should have handled the negative integer or raised an error."
+		assert(type(result) is str), \
+			"foo_bar_baz(" + str(i) + ") should return a string.  Got: " + str(type(result))
+		
+		assert(result == ""), \
+			"Expected: ||\n     Got: |" + result + "|"
 
 def test_positives():
 	outputUpToLimit:str = [
